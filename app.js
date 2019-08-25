a54d687eb6b405c55476419ac73c850c8e3ef007
Kristiawan Adi L <lutfi@ainosi.co.id>
2019-08-26 04:43:23 +0700

    add config 2

--- app.js
+++ app.js
@@ -1,111 +1,111 @@
-// var express = require('express')
-// var app = express()
-// var randomstring = require("randomstring");
-// var cookieSession = require('cookie-session')
-// const axios = require('axios');
-// var FormData = require('form-data');
+var express = require('express')
+var app = express()
+var randomstring = require("randomstring");
+var cookieSession = require('cookie-session')
+const axios = require('axios');
+var FormData = require('form-data');
 
 
-// var clientId = "ece071df68ed64b2"
-// var clientSecret = "c85bafe569bf4e208608e4a86c819e3b"
-// var redirectUri = "http://demo1.store/callback"
-// var responseType = "code"
-// var state = ""
-// var scope = "all"
-// var grantType = "authorization_code"
-// var tokenUrl = "https://racksso.herokuapp.com/api/v1/oauth2/token"
-// var userInfo = "https://racksso.herokuapp.com/api/v1/userinfo"
-// var responseToken
+var clientId = "ece071df68ed64b2"
+var clientSecret = "c85bafe569bf4e208608e4a86c819e3b"
+var redirectUri = "http://demo1.store/callback"
+var responseType = "code"
+var state = ""
+var scope = "all"
+var grantType = "authorization_code"
+var tokenUrl = "https://racksso.herokuapp.com/api/v1/oauth2/token"
+var userInfo = "https://racksso.herokuapp.com/api/v1/userinfo"
+var responseToken
 
-// function authorizeUrl(pState){
-//   return "https://racksso.herokuapp.com/oauth2/authorize?client_id="+clientId+"&redirect_uri="+redirectUri+"&response_type="+responseType+"&scope="+scope+"&state="+pState
-// }
+function authorizeUrl(pState){
+  return "https://racksso.herokuapp.com/oauth2/authorize?client_id="+clientId+"&redirect_uri="+redirectUri+"&response_type="+responseType+"&scope="+scope+"&state="+pState
+}
  
-// app.set('view engine', 'pug')
-// app.use(cookieSession({
-//   name: 'csid',
-//   keys: ['!secret!@#'],
-// }))
-// app.use('/static', express.static('public'))
+app.set('view engine', 'pug')
+app.use(cookieSession({
+  name: 'csid',
+  keys: ['!secret!@#'],
+}))
+app.use('/static', express.static('public'))
 
-// app.get('/', function (req, res) {
-//   if (req.session.loginstate == undefined){
-//     res.redirect(302, '/login')
-//   }else{
-//     var token = req.session.token
-//     console.log(token)
+app.get('/', function (req, res) {
+  if (req.session.loginstate == undefined){
+    res.redirect(302, '/login')
+  }else{
+    var token = req.session.token
+    console.log(token)
 
-//     axios.create({headers: {'Authorization': 'Bearer '+token.access_token}}).get(userInfo)
-//     .then(function (response) {
-//       var data = {
-//         token: token,
-//         data : response.data
-//       }
+    axios.create({headers: {'Authorization': 'Bearer '+token.access_token}}).get(userInfo)
+    .then(function (response) {
+      var data = {
+        token: token,
+        data : response.data
+      }
       
-//       res.render('index', data)
-//     })
-//     .catch(function (error) {
-//       console.log(error.message);
-//       return res.status(400).send("application error. \n to get parameter from auth server")
-//     });
-//   }  
-// })
-// app.get('/login', function (req, res) {
-//   if (req.session.loginstate != undefined){
-//     res.redirect(302, '/')
-//   }
+      res.render('index', data)
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return res.status(400).send("application error. \n to get parameter from auth server")
+    });
+  }  
+})
+app.get('/login', function (req, res) {
+  if (req.session.loginstate != undefined){
+    res.redirect(302, '/')
+  }
 
-//   res.render('login', { loginDashboard: '/auth/dashboard' })
-// })
+  res.render('login', { loginDashboard: '/auth/dashboard' })
+})
 
-// app.get('/auth/dashboard', function (req, res) {
-//   state = randomstring.generate(7)
-//   res.redirect(302, authorizeUrl(state))
-// })
+app.get('/auth/dashboard', function (req, res) {
+  state = randomstring.generate(7)
+  res.redirect(302, authorizeUrl(state))
+})
 
-// app.get('/callback', function (req, res) {
-//   var qCode = req.query.code
-//   var qState = req.query.state
-//   console.log("code: ", qCode)
-//   console.log("state: ",qState)
-//   if (qState == state && qCode != undefined){
-//     var formData = new FormData()
-//     formData.append('grant_type', grantType)
-//     formData.append('client_id', clientId)
-//     formData.append('client_secret', clientSecret)
-//     formData.append('redirect_uri', redirectUri)
-//     formData.append('code', qCode)
-//     formData.append('scope', scope)
+app.get('/callback', function (req, res) {
+  var qCode = req.query.code
+  var qState = req.query.state
+  console.log("code: ", qCode)
+  console.log("state: ",qState)
+  if (qState == state && qCode != undefined){
+    var formData = new FormData()
+    formData.append('grant_type', grantType)
+    formData.append('client_id', clientId)
+    formData.append('client_secret', clientSecret)
+    formData.append('redirect_uri', redirectUri)
+    formData.append('code', qCode)
+    formData.append('scope', scope)
 
-//     axios.create({headers: formData.getHeaders()}).post(tokenUrl, formData)
-//     .then(function (response) {
-//       req.session.loginstate=state
-//       console.log(JSON.stringify(response.data));
-//       responseToken = response.data
-//       state = null
-//       req.session.token = responseToken
-//       console.log(responseToken)
-//       res.redirect(302, '/')
-//     })
-//     .catch(function (error) {
-//       console.log(error.message);
-//       return res.status(400).send("application error. \n to get parameter from auth server")
-//     });
-//   } else{
-//     res.status(400).send("application error. \n to get parameter from auth server")
-//   }
-// })
+    axios.create({headers: formData.getHeaders()}).post(tokenUrl, formData)
+    .then(function (response) {
+      req.session.loginstate=state
+      console.log(JSON.stringify(response.data));
+      responseToken = response.data
+      state = null
+      req.session.token = responseToken
+      console.log(responseToken)
+      res.redirect(302, '/')
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return res.status(400).send("application error. \n to get parameter from auth server")
+    });
+  } else{
+    res.status(400).send("application error. \n to get parameter from auth server")
+  }
+})
 
-// app.get('/logout', function (req, res) {
-//   req.session = undefined
+app.get('/logout', function (req, res) {
+  req.session = undefined
   
-//   res.redirect(302, '/login')
-// })
+  res.redirect(302, '/login')
+})
  
-// console.log('start in port 3001')
-// app.listen(3001)
-'use strict';
+console.log('start in port 3001')
+app.listen(8080)
+// 'use strict';
 
-const app = require('./express/server');
+// const app = require('./express/server');
 
-app.listen(3001, () => console.log('Local app listening on port 3001!'));
+// app.listen(3001, () => console.log('Local app listening on port 3001!'));
